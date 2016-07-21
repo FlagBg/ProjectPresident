dd8c59b5ac67ab63dea89eb4c4b1f1fde1bb3908
Angel Bayraktarov <angel.bayraktarov@plovdivsoftware.com>
2016-07-21 14:57:25 +0300

    start building the edit adding javascript to get peo_id
    start building the edit adding javascript to get peo_id

--- View/PresidentsTemplate.php
+++ View/PresidentsTemplate.php
@@ -148,6 +148,7 @@
 			</div>
 		</div>
 	</div>
+	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 							<script type="text/javascript">
 								$(document).ready(function()
